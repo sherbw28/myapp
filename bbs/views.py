4c7f0cb735f274db385d2caddf9b186baebabd54
eisuke <sherbw28@outlook.jp>
2022-02-01 23:28:08 +0900

    2/1の変更

--- bbs/views.py
+++ bbs/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from http.client import HTTPResponse
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Article
 
 def index(request):
+    articles = Article.objects.all()
     post ={
         "title": "これはタイトルです",
-        'players': ["勇者", "戦士", "魔法使い"]
+        'players': ["勇者", "戦士", "魔法使い"],
+        'articles': articles
     }
     return render(request, 'bbs/index.html', post)
+
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    return render(request, 'bbs/detail.html', {'post': article})
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,13 @@
 from unicodedata import name
+from django import views
 from django.contrib import admin
 from django.urls import path
-from bbs.views import index
+from bbs.views import index, detail
+
+app_name = 'bbs'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('<int:id>/', detail, name='detail')
 ]
